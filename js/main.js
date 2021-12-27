7f6aed1271c2306b0de2b7539b3b8c1300ddc3d4
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2021-12-27 13:43:12 -0500

    Move away from node, implement basic search

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const options = {
+  isCaseSensitive: false,
+  includeScore: false,
+  shouldSort: true,
+  includeMatches: false,
+  findAllMatches: false,
+  minMatchCharLength: 1,
+  location: 0,
+  threshold: 0.6,
+  distance: 100,
+  useExtendedSearch: false,
+  ignoreLocation: false,
+  ignoreFieldNorm: false,
+  fieldNormWeight: 1,
+  keys: [
+    "title",
+    "alt",
+    "comic",
+    "bonus"
+  ]
+};
+
+const list = [
+  {
+    "title": "2020-01-01",
+    "alt": "this is the alt text",
+    "comic": "The actual tect in the comic",
+    "bonus": "bonus pa an l tex t"
+  }
+];
+
+const fuse = new Fuse(list, options);
+
+function updateSearch() {
+  var searchbox = document.getElementById("searchbox");
+  var resultsBox = document.getElementById("results");
+  resultsBox.innerHTML = "";
+  console.log(searchbox.value);
+  var fuseResults = fuse.search(searchbox.value);
+  console.log(fuseResults);
+  for (var i = 0; i < fuseResults.length; i++) {
+    var fuseResult = fuseResults[i];
+    var resultItemLink = document.createElement("a");
+    var resultCard = document.createElement("div");
+    resultCard.classList.add("box-shadow")
+    resultItemLink.appendChild(resultCard);
+    resultItemLink.href = "https://smbc-comics.com/comic/" + fuseResult.item.title;
+    resultCard.id = "card";
+    var resultTitle = document.createElement("h2");
+    var resultAlt = document.createElement("h3");
+    resultTitle.innerText=fuseResult.item.title;
+    resultAlt.innerText=fuseResult.item.alt;
+    resultCard.appendChild(resultTitle);
+    resultCard.appendChild(resultAlt);
+    resultsBox.appendChild(resultItemLink);
+  }
+}
