f4d5dcdd5c40fd7e6d6b09a3d84b1d76b62ac9da
Sambhav Saggi <17993169+sambhavsaggi@users.noreply.github.com>
2021-12-27 23:35:43 -0500

    Improve search responsiveness a little

--- docs/js/main.js
+++ docs/js/main.js
@@ -21,28 +21,48 @@ const options = {
 };
 
 const fuse = new Fuse(list, options);
+var timer;
+var prevsearch = '';
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("dom loaded");
+  document.querySelector("#searchbox").addEventListener('keyup', updateSearch);
+});
 
 function updateSearch() {
-  var searchbox = document.getElementById("searchbox");
-  var resultsBox = document.getElementById("results");
-  resultsBox.innerHTML = "";
-  console.log(searchbox.value);
-  var fuseResults = fuse.search(searchbox.value);
-  var maxlen = Math.min(100, fuseResults.length);
-  for (var i = 0; i < maxlen; i++) {
-    var fuseResult = fuseResults[i];
-    var resultItemLink = document.createElement("a");
-    var resultCard = document.createElement("div");
-    resultCard.classList.add("box-shadow")
-    resultItemLink.appendChild(resultCard);
-    resultItemLink.href = "https://smbc-comics.com/comic/" + fuseResult.item.title;
-    resultCard.id = "card";
-    var resultTitle = document.createElement("h2");
-    var resultAlt = document.createElement("h3");
-    resultTitle.innerText=fuseResult.item.title;
-    resultAlt.innerText=fuseResult.item.alt;
-    resultCard.appendChild(resultTitle);
-    resultCard.appendChild(resultAlt);
-    resultsBox.appendChild(resultItemLink);
+  try {
+    timer.clearTimeout();
+  } catch {
+    console.log("Starting new search");
+  } finally {
+    timer = setTimeout(function() {    
+      var searchbox = document.getElementById("searchbox");
+      var resultsBox = document.getElementById("results");
+      if (searchbox.value == prevsearch) {
+        return;
+      } else {
+        prevsearch = searchbox.value;
+      }
+      console.log(searchbox.value);
+      var fuseResults = fuse.search(searchbox.value);
+      var maxlen = Math.min(100, fuseResults.length);
+      resultsBox.innerHTML = "";
+      for (var i = 0; i < maxlen; i++) {
+        var fuseResult = fuseResults[i];
+        var resultItemLink = document.createElement("a");
+        var resultCard = document.createElement("div");
+        resultCard.classList.add("box-shadow")
+        resultItemLink.appendChild(resultCard);
+        resultItemLink.href = "https://smbc-comics.com/comic/" + fuseResult.item.title;
+        resultCard.id = "card";
+        var resultTitle = document.createElement("h2");
+        var resultAlt = document.createElement("h3");
+        resultTitle.innerText=fuseResult.item.title;
+        resultAlt.innerText=fuseResult.item.alt;
+        resultCard.appendChild(resultTitle);
+        resultCard.appendChild(resultAlt);
+        resultsBox.appendChild(resultItemLink);
+      }
+    }, 1000);
   }
 }
